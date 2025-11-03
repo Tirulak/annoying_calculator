34cfe53e9aa2f2d639c4a2fc95a1387ab121d7e7
Milosz <miloszstalonczyk@gmail.com>
2025-11-03 20:41:32 +0100

    Created calculator class. Implemented basic math

---
+++ main.py
@@ -0,0 +1,78 @@
+import math
+import os
+
+
+# simple calculator app. No offense
+class NonOffensiveCalculator:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    #simple math. non-offensive:
+    def addition(self):
+        return self.num1 + self.num2
+
+    def subtraction(self):
+        return self.num1 - self.num2
+
+    def multiplication(self):
+        return self.num1 * self.num2
+
+    def division(self):
+        return self.num1 / self.num2
+
+    def full_division(self):
+        return self.num1 // self.num2
+
+    def power(self):
+        return self.num1 ** self.num2
+
+print('############### CALCULATOR OF TWO NUMBERS ###############\n \n'
+      'To list possible operations, type "help"')
+
+
+user_input = input('Awaiting user input... ')
+
+if user_input == "help":
+    print(' \nPossible operations: \n'
+          '1 - addition \n'
+          '2 - subtraction \n'
+          '3 - multiplication \n'
+          '4 - division \n'
+          '5 - full_division \n'
+          '6 - power \n'
+          '7 - exit the program... \n'
+          'Just type a number. \n'
+          )
+
+
+
+operation = input('What operation do you want to EXECUTE?')
+
+a = int(input('Enter first number you want to calculate: '))
+b = int(input('Enter second number you want to calculate: '))
+calculator = NonOffensiveCalculator(a, b)
+
+# Now the chain of if elifelifleleleleliflelelfi
+
+if operation == '1':
+    print(calculator.addition())
+
+elif operation == '2':
+    print(calculator.subtraction())
+
+elif operation == '3':
+    print(calculator.multiplication())
+
+elif operation == '4':
+    print(calculator.division())
+
+elif operation == '5':
+    print(calculator.full_division())
+
+elif operation == '6':
+    print(calculator.power())
+
+elif operation == '7':
+    exit()
+
