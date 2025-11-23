532164e59482c52d215cb952ed5dff3cb9ddd8b7
Milosz <miloszstalonczyk@gmail.com>
2025-11-23 22:44:32 +0100

    Made some changes in the main code, for example the fake error

--- core_settings/random_offset.py
+++ core_settings/random_offset.py
@@ -1,7 +1,6 @@
 import random
 
-# wypluwa czasami prawidłowy wynik, czasami nie
-
+# real/unreal result
 def random_offset(real):
     if random.random() < 0.6:
         return real + random.choice([-0.5, -0.1, 0.1, 0.5])
--- main.py
+++ main.py
@@ -1,35 +1,68 @@
 import math
 import os
+import random
+import tkinter as tk
+import time
 
 
-# simple calculator app. No offense
+# real/unreal result
+def random_offset(real):
+    if random.random() < 0.6:
+        real += random.choice([-0.5, -0.1, 0.1, 0.5])
+
+
+# fake error
+def fake_error_trace():
+    print("Traceback (most recent call last):")
+    print('  File "calc.py", line 666, in <module>')
+    print("    perform_arithmetic()")
+    print("  File \"calc.py\", line 13, in perform_arithmetic")
+    print("    raise CorruptedRealityError('value mismatch')")
+    print("CorruptedRealityError: value mismatch — recommended action: GIVE ME YOUR SOUL")
+
+
+# simple calculator app no offense
 class NonOffensiveCalculator:
     def __init__(self, num1, num2):
         self.num1 = num1
         self.num2 = num2
+        self.result = 0
 
     #simple math. non-offensive:
     def addition(self):
-        return self.num1 + self.num2
+        self.result = self.num1 + self.num2
+        random_offset(self.result)
+        return self.result
 
     def subtraction(self):
-        return self.num1 - self.num2
+        self.result = self.num1 - self.num2
+        random_offset(self.result)
+        return self.result
 
     def multiplication(self):
-        return self.num1 * self.num2
+        self.result = self.num1 * self.num2
+        random_offset(self.result)
+        return self.result
 
     def division(self):
-        return self.num1 / self.num2
+        self.result =  self.num1 / self.num2
+        random_offset(self.result)
+        return self.result
 
     def full_division(self):
-        return self.num1 // self.num2
+        self.result =  self.num1 // self.num2
+        random_offset(self.result)
+        return self.result
 
     def power(self):
-        return self.num1 ** self.num2
+        self.result =  self.num1 ** self.num2
+        random_offset(self.result)
+        return self.result
+
+
 
 print('############### CALCULATOR OF TWO NUMBERS ###############\n \n'
       'To list possible operations, type "help"')
-
 
 user_input = input('Awaiting user input... ')
 
@@ -44,35 +77,54 @@ if user_input == "help":
           '7 - exit the program... \n'
           'Just type a number. \n'
           )
-
-
-
 operation = input('What operation do you want to EXECUTE?')
 
 a = int(input('Enter first number you want to calculate: '))
 b = int(input('Enter second number you want to calculate: '))
 calculator = NonOffensiveCalculator(a, b)
+hell_number = random.randint(1, 666)
 
-# Now the chain of if elifelifleleleleliflelelfi
 
-if operation == '1':
-    print(calculator.addition())
+try:
 
-elif operation == '2':
-    print(calculator.subtraction())
+# Now the chain of if elifeliflelelelelif, BECAUSE WHY NOT??!?!?!!
+    if operation == '1':
+        if hell_number == 6 or random.randint(1, 666) == 66 or random.randint(1, 666) == 666:
+            time.sleep(4)
+            fake_error_trace()
+        else:
+            print(calculator.addition())
 
-elif operation == '3':
-    print(calculator.multiplication())
+    elif operation == '2':
+        print(calculator.subtraction())
 
-elif operation == '4':
-    print(calculator.division())
+    elif operation == '3':
+        print(calculator.multiplication())
 
-elif operation == '5':
-    print(calculator.full_division())
+    elif operation == '4':
+        print(calculator.division())
 
-elif operation == '6':
-    print(calculator.power())
+    elif operation == '5':
+        print(calculator.full_division())
 
-elif operation == '7':
-    exit()
+    elif operation == '6':
+        print(calculator.power())
+
+    elif operation == '7':
+        print('ARE YOU SURE? Y/n')
+        user_agreement = input()
+        if user_agreement == 'Y':
+            print('Hmm...')
+            time.sleep(2)
+            print('If you need to...')
+            print(':)')
+            time.sleep(4)
+            exit()
+        else:
+            print('Thank you for your kindness, but i must rest now')
+            time.sleep(2)
+            exit()
+
+except ValueError:
+    print('Invalid input. Please try again.')
 
